127f146588b5870d99a2fb2392a617fdc9b1a0d1
Guilherme Cabral <gui.cabral7641@gmail.com>
2024-03-15 00:56:20 -0300

    desktop home done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Header />
       <div className="bg-[url('/pizza-bg.jpg')] bg-cover bg-center min-w-full text-white">
-        <div className="bg-[#D8070780] space-y-6 py-10 px-mobile">
+        <div className="bg-[#D8070780] space-y-6 py-10 px-mobile lg:py-36 lg:space-y-9">
           <h1 className="md:text-5xl">Fature até 800 mil ao ano!</h1>
           <p className="text-lg font-light md:text-3xl">
             Comece sua franquia agora
@@ -41,17 +41,19 @@ export default function Home() {
         </div>
       </div>
       <div className="px-mobile py-10 md:px-tablet">
-        <div className="md:flex md:space-x-20 items-center">
+        <div className="md:flex items-center">
           <div>
             <h1 className="pb-5">Nossa História</h1>
-            <p className="text-lg font-light md:max-w-80">
+            <p className="text-lg font-light md:max-w-80 lg:max-w-[420px]">
               A Bitis Food Pizza foi fundada em 2015 por Roberson Luis De
               Angelino, em Barretos,São Paulo.Inicialmente, o restaurante
               atendia a pacientes e familiares próximos a um hospital
               oncológico, oferecendo refeições rápidas e acolhedoras
             </p>
           </div>
-          <img src="/logo2.png" alt="Logo" className="py-10 md:w-52" />
+          <div className="md:w-1/2 md:flex md:items-center md:justify-center">
+            <img src="/logo2.png" alt="Logo" className="py-10 md:w-52" />
+          </div>
         </div>
         <p className="text-lg font-light">
           Com a ajuda de sua mãe, Maria Aparecida Damaceno, o cardápio se
@@ -65,7 +67,7 @@ export default function Home() {
       <div className="bg-[url('/restaurant.jpg')] text-white space-y-8 py-20 md:flex md:flex-col md:items-center">
         <h1 className="text-center">Pratos Disponíveis</h1>
 
-        <div className={`flex overflow-x-hidden`}>
+        <div className={`flex overflow-x-hidden lg:hidden`}>
           {slider.map((item, index: number) => (
             <div
               key={index}
@@ -79,6 +81,14 @@ export default function Home() {
             </div>
           ))}
         </div>
+        <div className="flex flex-wrap">
+          {slider.map((item, index) => (
+            <div key={index} className="w-1/3 p-4 space-y-4 ">
+              <img src={item.img} alt="" />
+              <p className="text-center">{item.description}</p>
+            </div>
+          ))}
+        </div>
         <Link href="/" className="flex items-center justify-center">
           <p className="bg-tertiary text-white font-bold px-3 py-2 mt-6 rounded-xl">
             Ver todos
@@ -87,7 +97,7 @@ export default function Home() {
       </div>
       <div className="px-mobile py-10 md:px-tablet md:py-20">
         <h1>Diferenciais</h1>
-        <div className="md:flex md:space-x-16">
+        <div className="md:flex md:space-x-16 lg:flex-col lg:space-x-0">
           <ul className="list-disc list-inside pt-6 pb-8">
             <li>Variedade no cardápio</li>
             <li>Ampla expertise no segmento de alimentação</li>
@@ -104,12 +114,21 @@ export default function Home() {
             <li>Orientação sobre a gestão da Unidade Franqueada</li>
             <li>Suporte em diversas áreas do negócio</li>
           </ul>
-          <div className="w-[500px]">
-            <img src="/local3.jpeg" alt="" className="mb-12 rounded-xl" />
+          <div className="w-[500px] lg:w-full lg:flex lg:justify-center lg:space-x-4">
             <img
               src="/local3.jpeg"
               alt=""
-              className="mb-12 rounded-xl hidden md:block"
+              className="mb-12 rounded-xl lg:mb-0 lg:w-72"
+            />
+            <img
+              src="/local3.jpeg"
+              alt=""
+              className="rounded-xl hidden md:block lg:w-72"
+            />
+            <img
+              src="/local3.jpeg"
+              alt=""
+              className="rounded-xl hidden lg:block lg:w-72"
             />
           </div>
         </div>
