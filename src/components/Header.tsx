6a078e91323d3bfd09c1c073be93657adde154c4
Guilherme Cabral <gui.cabral7641@gmail.com>
2024-07-04 21:02:46 -0300

    fix(header): fixed header width to adjust alignment correctly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,57 +14,62 @@ export default () => {
   return (
     <>
       <Sidebar isOpen={tappedSidebar} setIsOpen={setTappedSidebar} />
-      <div className="bg-primary shadow-md h-20 z-10 w-full flex items-center justify-between lg:justify-center px-mobile tablet:px-tablet desktop:px-desktop fixed font-title text-black">
-        <div className="hidden desktop:flex flex-1 space-x-8 text-black items-center">
-          <Link href="/">
-            <h2 className="hover:text-secondaryHover duration-75 hover:scale-110">
-              Home
-            </h2>
-          </Link>
-          <Link href="/cardapio">
-            <h2 className="hover:text-secondaryHover duration-75 hover:scale-110">
-              Cardápio
-            </h2>
-          </Link>
-          <Link href="/franquia">
-            <h2 className="hover:text-secondaryHover duration-75 hover:scale-110">
-              Quero Faturar
-            </h2>
-          </Link>
-        </div>
-        <Link href={"/"}>
-          <div className="pt-10 desktop:pt-16 justify-center items-center duration-75 hover:scale-110">
-            <img src="/logo.svg" className="w-24 desktop:w-28" />
+      <div className="bg-primary shadow-md shadow-gray-800/60 h-20 z-10 w-full flex items-center justify-between lg:justify-center px-mobile tablet:px-tablet desktop:px-desktop fixed font-title text-black">
+        <div className="flex w-full justify-between desktop:justify-center desktop:w-[1100px]">
+          <div className="hidden desktop:flex flex-1 space-x-8 text-black items-center">
+            <Link href="/">
+              <h2 className="hover:text-secondaryHover duration-75 hover:scale-110">
+                Home
+              </h2>
+            </Link>
+            <Link href="/cardapio">
+              <h2 className="hover:text-secondaryHover duration-75 hover:scale-110">
+                Cardápio
+              </h2>
+            </Link>
+            <Link href="/franquia">
+              <h2 className="hover:text-secondaryHover duration-75 hover:scale-110">
+                Quero Faturar
+              </h2>
+            </Link>
           </div>
-        </Link>
-        <div className="hidden flex-1 desktop:flex justify-end items-center space-x-8">
-          <Link
-            href="https://api.whatsapp.com/send?phone=5517981733913"
-            target="_blank"
-          >
-            <IoLogoWhatsapp className="size-7 hover:text-secondaryHover duration-75 hover:scale-110" />
-          </Link>
-          <Link href="https://www.facebook.com/bitisfoodpizza" target="_blank">
-            <FaFacebookF className="size-7 hover:text-secondaryHover duration-75 hover:scale-110" />
-          </Link>
-          <Link
-            href="https://www.instagram.com/bitisfoodpizza/"
-            target="_blank"
-          >
-            <IoLogoInstagram className="size-7 hover:text-secondaryHover duration-75 hover:scale-110" />
-          </Link>
-          <Link href="/contato">
-            <div
-              className="bg-secondary
-             px-8 py-2 flex flex-row justify-center items-center rounded-lg text-white hover:scale-110 duration-75 hover:bg-secondaryHover"
-            >
-              <h2>Contato</h2>
+          <Link href={"/"}>
+            <div className="pt-10 desktop:pt-16 justify-center items-center duration-75 hover:scale-110">
+              <img src="/logo.svg" className="w-24 desktop:w-28" />
             </div>
           </Link>
+          <div className="hidden flex-1 desktop:flex justify-end items-center space-x-8">
+            <Link
+              href="https://api.whatsapp.com/send?phone=5517981733913"
+              target="_blank"
+            >
+              <IoLogoWhatsapp className="size-7 hover:text-secondaryHover duration-75 hover:scale-110" />
+            </Link>
+            <Link
+              href="https://www.facebook.com/bitisfoodpizza"
+              target="_blank"
+            >
+              <FaFacebookF className="size-7 hover:text-secondaryHover duration-75 hover:scale-110" />
+            </Link>
+            <Link
+              href="https://www.instagram.com/bitisfoodpizza/"
+              target="_blank"
+            >
+              <IoLogoInstagram className="size-7 hover:text-secondaryHover duration-75 hover:scale-110" />
+            </Link>
+            <Link href="/contato">
+              <div
+                className="bg-secondary
+             px-8 py-2 flex flex-row justify-center items-center rounded-lg text-white hover:scale-110 duration-75 hover:bg-secondaryHover"
+              >
+                <h2>Contato</h2>
+              </div>
+            </Link>
+          </div>
+          <button onClick={() => setTappedSidebar(true)}>
+            <IoMenu className="h-full size-10 desktop:hidden" />
+          </button>
         </div>
-        <button onClick={() => setTappedSidebar(true)}>
-          <IoMenu className="h-full size-10 desktop:hidden" />
-        </button>
       </div>
       <div className="h-20" />
     </>
